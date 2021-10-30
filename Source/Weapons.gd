c5ae6a95240cde9886c32118b7b0e7244039364c
StarboySean <seandharmawan@gmail.com>
2021-10-29 17:55:43 -0700

    grenade

--- Source/Weapons.gd
+++ Source/Weapons.gd
@@ -1,12 +1,13 @@
 extends ItemList
 onready var viewport_rect := get_viewport_rect()
-
+const Explosion := preload("res://Source/Explosion.tscn")
 enum Weapon{
 	UNARMED = -1,
 	RIFLE,
-	ROCKET	
+	GRENADE	
 }
-func fire():
+func fire(at: Vector2):
+	var parent = get_parent()
 	var items := get_selected_items()
 	if not items:
 		return
@@ -15,18 +16,24 @@ func fire():
 	var item := items[0]
 	match item:
 		Weapon.RIFLE:
-			pass
-		Weapon.ROCKET:
-			pass
+			$RayCast2D.cast_to = at - $RayCast2D.position
+			$RayCast2D.force_raycast_update()
+			if $RayCast2D.is_colliding():
+				var collider = $RayCast2D.get_collider()
+				collider.queue_free()
+		Weapon.GRENADE:
+			var explosion := Explosion.instance()
+			explosion.position = at
+			parent.add_child(explosion)
 func _ready():
 	add_item("rifle")
-	add_item("rocket")
+	add_item("grenade")
 	$RayCast2D.position.y = viewport_rect.size.y
 	$RayCast2D.position.x = (viewport_rect.size.x - rect_size.x) *0.5 + rect_size.x
 
 func _input(event):
-	if event.is_action_pressed("fire"):
-		fire()
+	if event is InputEventMouseButton and event.is_action_pressed("fire"):
+		fire(event.position)
 
 
 	
