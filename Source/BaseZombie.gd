624f54d73e64c6ec96fe4bb3c3dfce63d2d023ba
StarboySean <seandharmawan@gmail.com>
2021-12-09 18:11:02 -0800

    death animations working

--- Source/BaseZombie.gd
+++ Source/BaseZombie.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 export var speed := 1.0
 var rng = RandomNumberGenerator.new()
 var offset := 0.0
+var alive := true
 
 func _ready():
 	rng.randomize()
@@ -17,12 +18,28 @@ func _process(delta):
 func _on_AnimatedSprite_frame_changed():
 	move_and_collide(Vector2.DOWN * speed)
 func die():
+	if alive:
+		alive = false
+	else:
+		return
+	$AnimatedSprite.stop()
 	var tween = get_node("Tween")
 	tween.interpolate_property(self, "rotation_degrees",
-		0, 90, 0.3,
+		0, 90, 1,
 		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	tween.start()
 
 
 func _on_Tween_tween_completed(object, key):
+	if modulate.a:
+		var tween = get_node("Tween")
+		tween.interpolate_property(self, "modulate:a",
+			1, 0, 1,
+			Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+		tween.start()
+	else:
+		queue_free()
+
+
+func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
