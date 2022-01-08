739335d57738c2495e0d4ebd60918d0e939419fc
StarboySean <seandharmawan@gmail.com>
2022-01-07 17:58:44 -0800

    adding background

---
+++ Source/Background.gd
@@ -0,0 +1,23 @@
+tool
+extends Node2D
+
+const Grass := preload("res://Assets/grass (1).png")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var sprite : Sprite
+	
+	
+
+	var position := Vector2.ZERO
+	for y in 2:
+		for x in 3:
+			sprite = Sprite.new()
+			sprite.texture = Grass
+			sprite.position = position
+			position.x += Grass.get_size().x
+			add_child(sprite)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
